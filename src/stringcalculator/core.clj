49bcd3af2f07b824cc46f630e1df52f7ddb98e16
Dean Gerber <dean.gerber@gmail.com>
2017-03-03 21:13:37 +0200

    feature: should return 3 when given '//;\n1;2'.

--- src/stringcalculator/core.clj
+++ src/stringcalculator/core.clj
@@ -3,6 +3,7 @@
 
 (defn add
   [s]
-  (if (= s "")
-    0
-    (reduce + 0 (map #(Integer/parseInt %) (str/split s #",|\n")))))
+  (cond
+    (str/blank? s) 0
+    (str/starts-with? s "//") (reduce + 0 (map #(Integer/parseInt %) (str/split (second (str/split-lines s)) (re-pattern (subs (first (str/split-lines s)) 2)))))
+    :else (reduce + 0 (map #(Integer/parseInt %) (str/split s #",|\n")))))
