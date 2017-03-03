ed38c4206ef870c370c0227ca574ede07945083b
Dean Gerber <dean.gerber@gmail.com>
2017-03-03 20:54:48 +0200

    chore: initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject stringcalculator "0.1.0-SNAPSHOT"
+  :description "String Calculator Kata"
+  :url "https://github.com/deangerber/stringcalculator-kata-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
